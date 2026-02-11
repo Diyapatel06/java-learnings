a9e7d0a5c64be6274a4e8889c1b21f0e924a73c8
Diyapatel06 <diyapatel061006@gmail.com>
2026-02-11 14:07:54 +0530

    morning session done till here

---
+++ loopingStats.java
@@ -0,0 +1,27 @@
+class forloop{
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            System.out.print( i );
+        }
+    }
+}
+
+class evenNum{
+    public static void main(String[] args){
+        for(int i=0;i<=50;i=i+2){
+            System.out.println(i);
+        }
+    }
+}
+
+class dowhileExamp{
+    public static void main(String[] args){
+        int i=1;
+
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=5);
+    }
+}
+
